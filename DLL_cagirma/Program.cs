2256e873368da430e6ec9d68747f416b7013c0cb
salihkoc <salihkoc-e@users.noreply.github.com>
2021-12-23 19:41:26 +0300

    Add project files.

---
+++ DLL_cagirma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_cagirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DLL_Kullanimi.Class1 matematikislemler = new DLL_Kullanimi.Class1();
+            int topla = matematikislemler.toplama(4, 6);
+            Console.WriteLine("sonuc: "+topla);
+            Console.ReadLine();
+        }
+    }
+}
